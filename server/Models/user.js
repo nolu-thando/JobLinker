20115913e4164a80e6c06e0e4756da2b0cc96b4b
Noluthando Mphande <noluthandowendy2013@gmail.com>
2025-04-16 12:32:39 +0200

    working on authentication

---
+++ server/Models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: { type: String, enum: ['youth', 'recruiter'], required: true },
+});
+
+module.exports = mongoose.model('User', UserSchema);
